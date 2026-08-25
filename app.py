a9877802b7a03682c089550507ca35263ce28899
dileepyanamala0310-svg <dileepyanamala0310@gmail.com>
2026-08-25 10:53:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+@app.route("/")
+def home():
+    return "Hello, World! My Azure Web App is working!"
+ 
+if __name__ == "__main__":
+    app.run()
